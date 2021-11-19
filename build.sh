da801383f7d4ad5d229101ed28e96641508f1cd6
Jeremy Gustie <jeremy@stormforge.io>
2021-11-19 16:12:06 -0500

    Move the build script to the top level

--- build.sh
+++ build.sh
@@ -21,10 +21,22 @@ CHART_DIR="$(git rev-parse --show-toplevel)/charts"
 CHART_NAME="$(basename ${IMAGE_REPO})"
 CHART_VERSION="$(dockerlabel "org.opencontainers.image.version")"
 CHART_VERSION="${CHART_VERSION#v}"
-# TODO Versions of the Helm chart can exist _between_ application releases, how do we account for that?
 
 
-# Overwrite the Helm chart metadata using information from the image
+# Helper functions
+hungryCat() { cat "$1" && rm "$1"; }
+removeNamespace() { sed -i "" "/namespace: '{{ .Release.Namespace }}'/d" "$1"; }
+
+
+# Create a build area and populate it with all the manifests we need
+BUILD_DIR="$(git rev-parse --show-toplevel)/build"
+rm -rf "${BUILD_DIR}" && mkdir -p "${BUILD_DIR}"
+cp "$(git rev-parse --show-toplevel)/src/${CHART_NAME}/kustomization.yaml" "${BUILD_DIR}"
+stormforge generate install --bootstrap-role > "${BUILD_DIR}/resources.yaml"
+(cd "${BUILD_DIR}" && kustomize build --output ".")
+
+
+# Chart.yaml
 cat <<-EOF > "${CHART_DIR}/${CHART_NAME}/Chart.yaml"
 apiVersion: v2
 name: ${CHART_NAME}
@@ -39,7 +51,7 @@ icon: https://app.stormforge.io/img/logo.png
 appVersion: "$(dockerlabel "org.opencontainers.image.version")"
 EOF
 
-# Overwrite the values
+# values.yaml
 cat <<-EOF > "${CHART_DIR}/${CHART_NAME}/values.yaml"
 image:
   repository: ${IMAGE_REPO}
@@ -54,40 +66,37 @@ authorization:
   clientSecret: ""
 EOF
 
-
-# Generate the manifests
-cd "$(git rev-parse --show-toplevel)/src/${CHART_NAME}"
-rm -rf "manifests" && mkdir "manifests"
-stormforge generate install --bootstrap-role > "resources.yaml"
-kustomize build --output "manifests"
-
+# crds/*
 CRDS_DIR="${CHART_DIR}/${CHART_NAME}/crds"
-for f in "manifests/"*_*_customresourcedefinition_*; do mv "${f}" "${CRDS_DIR}/${f#*_*_*_}" ; done
+for f in "${BUILD_DIR}/"*_*_customresourcedefinition_*; do mv "${f}" "${CRDS_DIR}/${f#*_*_*_}" ; done
 
+# templates/deployment.yaml
 DEPLOYMENT_TEMPLATE="${CHART_DIR}/${CHART_NAME}/templates/deployment.yaml"
-mv "manifests/apps_v1_deployment_{{ .release.name }}-controller-manager.yaml" "${DEPLOYMENT_TEMPLATE}"
-sed -i "" "/namespace: '{{ .Release.Namespace }}'/d" "${DEPLOYMENT_TEMPLATE}"
+mv "${BUILD_DIR}/apps_v1_deployment_{{ .release.name }}-controller-manager.yaml" "${DEPLOYMENT_TEMPLATE}"
+removeNamespace "${DEPLOYMENT_TEMPLATE}"
 
+# templates/secret.yaml
 SECRET_TEMPLATE="${CHART_DIR}/${CHART_NAME}/templates/secret.yaml"
-mv "manifests/v1_secret_{{ .release.name }}-manager.yaml" "${SECRET_TEMPLATE}"
-sed -i "" "/namespace: '{{ .Release.Namespace }}'/d" "${SECRET_TEMPLATE}"
+mv "${BUILD_DIR}/v1_secret_{{ .release.name }}-manager.yaml" "${SECRET_TEMPLATE}"
+removeNamespace "${SECRET_TEMPLATE}"
 
+# templates/rbac.yaml
 RBAC_TEMPLATE="${CHART_DIR}/${CHART_NAME}/templates/rbac.yaml"
-hungryCat() { cat "$1" && rm "$1"; }
 echo "{{- if .Values.rbac.create -}}" > "${RBAC_TEMPLATE}"
-hungryCat "manifests/rbac.authorization.k8s.io_v1_clusterrole_{{ .release.name }}-manager-role.yaml" >> "${RBAC_TEMPLATE}"
+hungryCat "${BUILD_DIR}/rbac.authorization.k8s.io_v1_clusterrole_{{ .release.name }}-manager-role.yaml" >> "${RBAC_TEMPLATE}"
 echo "---" >> "${RBAC_TEMPLATE}"
-hungryCat "manifests/rbac.authorization.k8s.io_v1_clusterrolebinding_{{ .release.name }}-manager-rolebinding.yaml" >> "${RBAC_TEMPLATE}"
+hungryCat "${BUILD_DIR}/rbac.authorization.k8s.io_v1_clusterrolebinding_{{ .release.name }}-manager-rolebinding.yaml" >> "${RBAC_TEMPLATE}"
 echo "{{- if .Values.rbac.bootstrapPermissions }}" >> "${RBAC_TEMPLATE}"
 echo "---" >> "${RBAC_TEMPLATE}"
-hungryCat "manifests/rbac.authorization.k8s.io_v1_clusterrole_{{ .release.name }}-patching-role.yaml" >> "${RBAC_TEMPLATE}"
+hungryCat "${BUILD_DIR}/rbac.authorization.k8s.io_v1_clusterrole_{{ .release.name }}-patching-role.yaml" >> "${RBAC_TEMPLATE}"
 echo "---" >> "${RBAC_TEMPLATE}"
-hungryCat "manifests/rbac.authorization.k8s.io_v1_clusterrolebinding_{{ .release.name }}-patching-rolebinding.yaml" >> "${RBAC_TEMPLATE}"
+hungryCat "${BUILD_DIR}/rbac.authorization.k8s.io_v1_clusterrolebinding_{{ .release.name }}-patching-rolebinding.yaml" >> "${RBAC_TEMPLATE}"
 echo "{{- end -}}" >> "${RBAC_TEMPLATE}"
 echo "{{- end -}}" >> "${RBAC_TEMPLATE}"
 
 
 # Clean up (fail if we didn't explicitly consume everything)
-rm "resources.yaml"
-rm "manifests/v1_namespace_{{ .release.namespace }}.yaml"
-rmdir manifests
+rm "${BUILD_DIR}/kustomization.yaml"
+rm "${BUILD_DIR}/resources.yaml"
+rm "${BUILD_DIR}/v1_namespace_{{ .release.namespace }}.yaml"
+rmdir "${BUILD_DIR}"
